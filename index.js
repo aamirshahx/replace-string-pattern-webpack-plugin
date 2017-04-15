f365644226ed3d0d145bb4a65024b6071258fbb8
Aamir Shah <aamir.shah.92@gmail.com>
2017-04-15 13:38:00 +0500

    added support for array input

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 let ConcatSource = require('webpack-core/lib/ConcatSource');
 
 function ReplaceStringPatternPlugin(options) {
-	this.options = options;
+	if (options instanceof Array) {
+		this.options = options;
+	} else {
+		this.options = [options];
+	}
 }
 
 ReplaceStringPatternPlugin.prototype.apply = function (compiler) {
 	compiler.plugin('compilation', (compilation) => {
 		compilation.plugin('optimize-chunk-assets', (chunks, done) => {
 			let options = this.options;
-			chunks
-				.forEach(chunk =>
-					chunk.files
-						.filter(file => options.filePattern.test(file))
-						.forEach(file => {
-							let result = compilation.assets[file].source();
-							if (!options.skip && options.pattern && options.replacement) {
-								result = result.replace(options.pattern, options.replacement);
-							}
-							compilation.assets[file] = new ConcatSource(result);
-						})
+			options.forEach(option => {
+				chunks.forEach(chunk => chunk
+					.files
+					.filter(file => option.filePattern.test(file))
+					.forEach(file => {
+						let result = compilation.assets[file].source();
+						if (!option.skip && option.pattern && option.replacement) {
+							result = result.replace(option.pattern, option.replacement);
+						}
+						compilation.assets[file] = new ConcatSource(result);
+					})
 				);
+			});
 			done();
 		})
 	});
