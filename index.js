3a33c8f73e39184c43a6b2dbe2a2a960cb7927a4
aamir.hussain <aamir.hussain@colwiz.com>
2021-05-19 15:00:13 +0500

    updated plugin for webpack 5+

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
-let ConcatSource = require('webpack-core/lib/ConcatSource');
+const ConcatSource = require('webpack-core/lib/ConcatSource');
+const pluginName = 'ReplaceStringPatternWebpackPlugin';
 
-function ReplaceStringPatternPlugin(options) {
-	if (options instanceof Array) {
-		this.options = options;
-	} else {
-		this.options = [options];
+class ReplaceStringPatternWebpackPlugin {
+	constructor(options) {
+		if (options instanceof Array) {
+			this.options = options;
+		} else {
+			this.options = [options];
+		}
+	}
+
+	apply(compiler) {
+		if (typeof compiler.hooks !== 'undefined') {
+			compiler.hooks.compilation.tap(pluginName, this.handleCompilation.bind(this))
+		} else if (typeof compiler.plugin !== 'undefined') {
+			compiler.plugin('compilation', this.handleCompilation.bind(this))
+		}
+	}
+
+	handleCompilation(compilation) {
+		compilation.hooks.optimizeChunkAssets.tapAsync(pluginName, (chunks, done) => {
+			let options = this.options;
+			options.forEach(option => {
+				chunks.forEach(chunk => {
+					chunk.files
+						.filter(file => option.filePattern.test(file))
+						.forEach(file => {
+							console.log(file)
+							let result = compilation.assets[file].source();
+							if (!option.skip && option.pattern && option.replacement) {
+								result = result.replace(option.pattern, option.replacement);
+							}
+							compilation.assets[file] = new ConcatSource(result);
+						});
+				});
+			});
+			done();
+		});
 	}
 }
 
-ReplaceStringPatternPlugin.prototype.apply = function (compiler) {
-	compiler.plugin('compilation', (compilation) => {
-		compilation.plugin('optimize-chunk-assets', (chunks, done) => {
-			let options = this.options;
-			options.forEach(option => {
-				chunks.forEach(chunk => chunk
-					.files
-					.filter(file => option.filePattern.test(file))
-					.forEach(file => {
-						let result = compilation.assets[file].source();
-						if (!option.skip && option.pattern && option.replacement) {
-							result = result.replace(option.pattern, option.replacement);
-						}
-						compilation.assets[file] = new ConcatSource(result);
-					})
-				);
-			});
-			done();
-		})
-	});
-};
-
-module.exports = ReplaceStringPatternPlugin;
+module.exports = ReplaceStringPatternWebpackPlugin;
